0f03c8d3dc5dd29f96af0daca8ba0616e4d60293
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-06 07:37:40 +0600

    Firebase: firebase config file updated with secure

--- src/firbase/firbaseConfig.js
+++ src/firbase/firbaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDG-atKtAqKiJVBbK5do-3uNg9VpJr9c90",
-  authDomain: "travel-guru-webapp-with-auth.firebaseapp.com",
-  projectId: "travel-guru-webapp-with-auth",
-  storageBucket: "travel-guru-webapp-with-auth.appspot.com",
-  messagingSenderId: "384780252094",
-  appId: "1:384780252094:web:d0e8196597f728be4f3fef"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
